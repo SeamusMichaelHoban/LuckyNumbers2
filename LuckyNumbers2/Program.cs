b6651b6c5591a7f3b334fb7f88f4e1f856a1e40c
SeamusMichaelHoban <seamushoban2000@gmail.com>
2018-08-23 21:39:04 -0400

    first commit

---
+++ LuckyNumbers2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumbers2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a starting number");
+            int minNumber = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter a ending number");
+            int maxNumber = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine(minNumber + " " + maxNumber);
+
+            int[] userNum = new int[6];
+
+            Console.WriteLine("Please enter six numbers");
+
+            for (int i = 0; i < userNum.Length; i++)
+            {
+                int inputTest = int.Parse(Console.ReadLine());
+
+                while (inputTest < minNumber || inputTest >= maxNumber)
+                {
+                    Console.WriteLine("Please enter a value greater than {0} and less than the {1}", minNumber, maxNumber);
+                    inputTest = int.Parse(Console.ReadLine());
+                }
+
+                userNum[i] = inputTest;
+                //Console.WriteLine(userNum[i]);
+            } 
+                int[] ranArray = new int[6];
+                Random r = new Random();
+
+                for (int j = 0; j < ranArray.Length; j++)
+                {
+                    ranArray[j] = r.Next(minNumber, maxNumber);
+                    Console.WriteLine("Lucky Number: {0}" ,ranArray[j]);
+                }
+
+
+            }
+           
+    }
+}
